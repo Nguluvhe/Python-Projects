10679956faa1291a6b0c408fd5fed8ae8d9d026c
Proffesor Nguluvhe <pnguluvhe37@gmail.com>
2025-05-01 13:29:32 +0200

    Add column mapping

--- csvFileMerg.py
+++ csvFileMerg.py
@@ -1,5 +1,16 @@
 #Use the pandas module
 import pandas as pd
 
-df1 = pd.read_csv("F:\Project\Python\sample_items.csv")
-df2 = pd.read_csv("F:\Project\Python\Titan_Pricefile_MV.csv")
+df1 = pd.read_csv("F:\Project\Python\sample_items.csv", encoding="utf-8")
+df2 = pd.read_csv("F:\Project\Python\Titan_Pricefile_MV.csv", encoding="utf-8")
+
+column_map = {
+    "ItemNumber": "SKU",
+    "Item Description": "Item Name",
+    "Category Code": "Product Type",
+    "Purchase_Cost": "Purchase Rate"
+}
+
+
+
+
