20685197c5bab1136b48182f86f38126a7a5021d
Proffesor Nguluvhe <pnguluvhe37@gmail.com>
2025-05-01 12:08:30 +0200

    adding file path

--- csvFileMerg.py
+++ csvFileMerg.py
@@ -1,4 +1,5 @@
 #Use the pandas module
 import pandas as pd
 
-df1 = pd.read_csv()
+df1 = pd.read_csv("F:\Project\Python\sample_items.csv")
+df2 = pd.read_csv("F:\Project\Python\Titan_Pricefile_MV.csv")
