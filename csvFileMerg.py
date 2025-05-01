a8c070adc43139426edc56920755133f86d73403
Proffesor Nguluvhe <pnguluvhe37@gmail.com>
2025-05-01 14:02:00 +0200

    Done

--- csvFileMerg.py
+++ csvFileMerg.py
@@ -19,7 +19,10 @@ df2_rename = df2.rename(columns=column_map)
 #Dropping the columns i don't need before merging and added errors ignore so that it ignores error if column doesn't exist
 df2 = df2.drop(columns=column_drop, errors="ignore")
 
+#Merging the data and added the ignore_index so that the two files don't have they own index
 merge_df = pd.concat([df1, df2_rename], ignore_index=True)
+
+#For filling data when the cell is empty the ffill fills data with the forward data and bfill uses the backward data 
 merge_df = merge_df.fillna(method="ffill").fillna("bfill")
 
 merge_df.to_csv("F:\Project\Python\merge_csv.csv", index=False, encoding="utf-8")
