9f9420168d1488b0e86684aa8316dfefb3412d9b
Proffesor Nguluvhe <pnguluvhe37@gmail.com>
2025-05-19 05:40:56 +0200

    api requested

--- firstapicall.py
+++ firstapicall.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
-response = requests.get("https://api.stackexchange.com//2.3/answers?order=desc&sort=activity&site=stackoverflow")
+# answers api key response = requests.get("https://api.stackexchange.com//2.3/answers?order=desc&sort=activity&site=stackoverflow")
 
-print(response.json())
+response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+
+for data in response.json()['items']:
+    print(data['title'])
